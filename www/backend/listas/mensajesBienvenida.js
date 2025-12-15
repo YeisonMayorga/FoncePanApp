11f3f7703f6d60febfba178c6d4433660d783c5f
YeisonMayorga <yeisonfa.2003@gmail.com>
2025-12-15 15:27:07 -0500

    Se creo el js con los mensajes de bienvenida

---
+++ www/backend/listas/mensajesBienvenida.js
@@ -0,0 +1,30 @@
+// welcomeMessages.js
+
+export const welcomeMessages = {
+    morning: [
+      'Todo listo para comenzar la jornada 🥖',
+      'Empezamos el día con buena energía',
+      'Que tengas una jornada productiva',
+      'El sistema está listo para ti',
+      'Buen inicio de día',
+    ],
+  
+    afternoon: [
+      'Sigamos avanzando con el trabajo',
+      'Todo en orden para continuar',
+      'Buen momento para seguir gestionando',
+      'La jornada sigue en marcha',
+      'Continuamos con las actividades',
+    ],
+  
+    night: [
+      'Últimos movimientos del día',
+      'Cerrando la jornada con orden',
+      'Gracias por el trabajo de hoy',
+      'Todo listo para finalizar',
+      'Hasta mañana, buen descanso',
+    ],
+  };
+
+  
+  
